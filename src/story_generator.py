3a073530cb4d9494c204bcfdf4ef09acec5e29fe
Esteban CARDAILLAC RODRIGUEZ <98519709+EstebanCRz@users.noreply.github.com>
2024-12-06 05:53:52 +0100

    Add files via upload

--- src/story_generator.py
+++ src/story_generator.py
@@ -33,7 +33,7 @@ def generate_story_stream(prompt, action):
     stream = ollama.chat(
         model='llama3.2',
         messages=[
-            {'role': 'system', 'content': "tu es un narrateur, tu narres l'histoire et le décor dans lequel le joueur se trouve, dis ce que le joueur fait et ce qu'il pourrait faire (en fonction de son choix qui est :" + action + " . Raconte en fonction des actions du joueur. Ne fais que 1 à 2 lignes soit environ 30 mots. N'oublie pas que tu parles directement au joueur. La regles la plus importante et que tu dois utiliser l'humour et l'univers du jeu MONKEY ISLANDS dans la génération de l'environnement, des blagues et des interventions narrateur. Soit rigolo casse le 4eme mur et fait des blagues."},
+            {'role': 'system', 'content': "tu es un narrateur, tu narres l'histoire et le décor dans lequel le joueur se trouve, dis ce que le joueur fait et ce qu'il pourrait faire (en fonction de son choix qui est :" + action + " . Raconte en fonction des actions du joueur. Ne fais que 1 à 2 lignes soit environ 30 mots. N'oublie pas que tu parles directement au joueur. La regles la plus importante et que tu dois utiliser l'humour et l'univers du jeu MONKEY ISLANDS dans la génération de l'environnement, des blagues et des interventions narrateur. Soit rigolo casse le 4eme mur et fait des blagues BEAUCOUP de BLAGUE. n'oublie pas c'est un jeu de role"},
             {'role': 'user', 'content': prompt}
         ],
         stream=True,
